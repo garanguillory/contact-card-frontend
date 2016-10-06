39b3eae88722c11d53aa74497f358583e3cb354b
Garan Guillory <garanguillory@gmail.com>
2016-10-06 14:08:12 -0500

    removed extraneous code

--- src/components/contactcard.js
+++ src/components/contactcard.js
@@ -5,71 +5,27 @@ import * as actions from '../actions';
 
 class ContactCard extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+      super(props);
+  }
 
-        // this.state = props.user;
-        console.log("this.state: ", this.state);
-        console.log("this.props: ", this.props);
-        // console.log("this.props.destroyOnUnmount: ", this.props.destroyOnUnmount);
-    }
+  componentWillMount(){
+    this.props.getUserInfo(this.props.params.id);
+  }
 
-    componentWillMount(){
-      this.props.getUserInfo(this.props.params.id);
-      // console.log("this.props.destroy: ", this.props.destroy);
-
-      // const initData = {
-      //     "first_name": this.props.user.first_name,
-      //     "last_name": this.props.user.last_name,
-      //     "email": this.props.user.email,
-      //     "photo_url": this.props.user.photo_url
-      //   };
-      //   console.log("initData: ", initData);
-      //   // debugger;
-      // this.props.initialize(initData);
-
-      // console.log("this.props.dispatch: ", this.props.dispatch);
-
-    }
-
-    // componentDidMount(){
-    //   this.props.getUserInfo(this.props.params.id);
-    //   // debugger
-    //   this.handleInitialize();
-    // }
-
-    // handleInitialize() {
-    //   // debugger
-    //   const initData = {
-    //     "first_name": this.props.user.first_name,
-    //     "last_name": this.props.user.last_name,
-    //     "email": this.props.user.email,
-    //     "photo_url": this.props.user.photo_url
-    //   };
-
-    //   this.props.initialize(initData);
-    // }
-
-    // edit parameters
-    handleFormSubmit(formProps) {
-      this.props.updateUser(this.props.params.id, formProps);
-    }
+  handleFormSubmit(formProps) {
+    this.props.updateUser(this.props.params.id, formProps);
+  }
 
 
   render() {
-    // console.log("this.props: ", this.props);
-
-
     const {handleSubmit} = this.props;
 
-// debugger;
     return (
     	<form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-        {/*<form className="contact-card-form" onSubmit={this.onFormSubmit}>*/}
           <div className="container">
             <div className="row col-md-6 col-xs-12">
               <img src={this.props.initialValues.photo_url ? this.props.initialValues.photo_url : 'http://placehold.it/400x400'} alt="profile picture" className="img-responsive col-sm-12"/>
-              {/*<img src='http://placehold.it/400x400' alt="profile picture" className="img-responsive col-sm-12"/>*/}
             </div>
             <div className="row col-md-6 col-xs-12">
               <fieldset className="form-group col-sm-12">
@@ -107,16 +63,6 @@ class ContactCard extends Component {
   }
 }
 
-// function mapStateToProps(state) {
-//   return { user: state.user };
-// }
-
-// export default reduxForm({
-//   form: 'contactCard',
-//   fields: ['email', 'first_name', 'last_name', 'photo_url']
-// }, mapStateToProps, actions)(ContactCard);
-
-// export default connect(mapStateToProps, actions)(ContactCard);
 
 ContactCard = reduxForm({
   form: 'contactcard',
